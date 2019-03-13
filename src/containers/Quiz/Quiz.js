9c77e7700fe3ce7c322876a234ce3432eb2cfa41
dude <dudehydra@gmail.com>
2019-03-13 22:42:36 +0300

    init project

---
+++ src/containers/Quiz/Quiz.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Quiz extends Component {
+    state = {
+
+    }
+    render() {
+        return (
+            <span>test</span>
+        )
+    }
+}
+
+export default Quiz;
