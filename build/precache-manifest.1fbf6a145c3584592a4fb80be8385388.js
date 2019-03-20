98edb2d3e96d303e10ee94bcff932c451c7dcbcf
insaf <insaf@ilartech.ru>
2019-03-20 15:18:44 +0300

    add build

---
+++ build/precache-manifest.1fbf6a145c3584592a4fb80be8385388.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d54b073e0c3bc9c8b33e2a4fd26ecfb4",
+    "url": "./static/media/arrow.d54b073e.svg"
+  },
+  {
+    "revision": "d961df0f84eafa286f92",
+    "url": "./static/css/main.c9aad70a.chunk.css"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "3447ecc37def9607564a",
+    "url": "./static/js/2.a1974066.chunk.js"
+  },
+  {
+    "revision": "e86ff5b613fb748bec7386521c70657e",
+    "url": "./static/media/check.e86ff5b6.svg"
+  },
+  {
+    "revision": "d961df0f84eafa286f92",
+    "url": "./static/js/main.e33ab9c2.chunk.js"
+  },
+  {
+    "revision": "a7fc66ed6bac6f0543fe91bdfe3ced2f",
+    "url": "./static/media/arrow-right-grey.a7fc66ed.svg"
+  },
+  {
+    "revision": "2998e3a0d6ae9978f38ee052e67da9ac",
+    "url": "./static/media/arrow-left.2998e3a0.svg"
+  },
+  {
+    "revision": "d43efa6b8630dd2a1dcc1b35e8f9db41",
+    "url": "./static/media/Rubik-Bold.d43efa6b.ttf"
+  },
+  {
+    "revision": "db5c7abfb1b57df0ae8ff1c236cbd810",
+    "url": "./static/media/Rubik-Regular.db5c7abf.ttf"
+  },
+  {
+    "revision": "6685c8716409c9d72308a83a10b5c6a1",
+    "url": "./index.html"
+  }
+];
